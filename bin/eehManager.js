b978eba25eb51657767c4ff91fa7fa10e500220b
eggsnham07 <eggsnham07@gmail.com>
2021-06-20 15:54:54 -0600

    Added/Updated Files

--- bin/eehManager.js
+++ bin/eehManager.js
@@ -1,6 +1,31 @@
 #!/usr/bin/env node
+const home = require("os").homedir()
 const server = require("../server")
+const open = require("open")
+const fs = require("fs")
 
 if(process.argv[2] == "start") {
+    const html = `<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Easy Express Hoster</title>
+        </head>
+        <body>
+            <h1>Welcome to the Home Page!</h1>
+        </body>
+    </html>`
+
+    if(!fs.existsSync(home + "\\Documents\\EasyExpressHoster\\Pages")) {
+        console.log("First time setup starting...")
+        if(!fs.existsSync(home + "\\Documents\\EasyExpressHoster")) {
+            fs.mkdirSync(home + "\\Documents\\EasyExpressHoster")
+            fs.mkdirSync(home + "\\Documents\\EasyExpressHoster\\Pages")
+        } else if(!fs.existsSync(home + "\\Documents\\EasyExpressHoster\\Pages")) {
+            fs.mkdirSync(home + "\\Documents\\EasyExpressHoster\\Pages")
+        }
+        fs.writeFileSync(home + "\\Documents\\EasyExpressHoster\\Pages", html)
+        console.log("Setup complete!")
+    }
     server(process.argv[3])
+    open('http://localhost:' + process.argv[3])
 }
