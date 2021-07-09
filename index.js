ee66ead2e9cc65717105e3803d821801a5899cf6
eggsnham07 <eggsnham07@gmail.com>
2021-07-09 13:08:28 -0600

    Added/Updated Files

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ module.exports = class Server {
         this.start = function() {
             if(this.dir == "~") {
                 app.use(express.static(process.cwd()))
+            } else {
+                app.use(express.static(this.dir))
             }
             app.listen(this.port, () => {
                 console.log(`Listening on port: ${this.port}`)
