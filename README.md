3de3da6dd8ef1eadfd79b711aec1768871ffecea
eggsnham07 <eggsnham07@gmail.com>
2021-07-14 13:09:50 -0600

    Added/Updated Files

--- README.md
+++ README.md
@@ -9,12 +9,15 @@ installation: `npm i easyexpresshoster -g`
 ```javascript
 const Server = require("@eggsnham07/easyexpresshoster")
 const server = new Server
+//tip: you can replace process.cwd with "~" currentfolder/pages
 server.dir = process.env.EEHDIR || process.cwd
-server.port = 5000
+server.port = 5000 
 server.start()
 ```
 
-you can also use a function called `server.post` or `server.get` and use it like express
+#### Node.js: Features
+You can use `server.usePrettyUrls()` to make /page.html become /page and if /page is a dir then /page.html will be redirected to /page/ 
+, and `server.post` or `server.get` work like express's express().post or express.get
 
 ### Terminal: Quick start
 if you are using this for the first time you should see something like this:
@@ -33,6 +36,9 @@ otherwise you wise you will see:
 Server listening on http://localhost:80
 Press CTRL + C to stop
 ```
+
+#### Terminal: Features
+If you do not want to use the default pages directory then you can add '~' at the end of the command to use the terminal's current directory
 
 
 all pages can found and edited in `C:\Users\you\Documents\EasyExpressHoster\Pages` or `/home/you/Documents/EasyExpressHoster/Pages`
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express")
+const fs = require("fs")
 const app = express()
 
 module.exports = class Server {
     constructor() {
         this.start = function() {
             if(this.dir == "~") {
-                this.dir = process.cwd()
+                this.dir = process.cwd() + "/pages"
             }
+
             app.use(express.static(this.dir))
 
             app.listen(this.port, () => {
@@ -20,6 +22,21 @@ module.exports = class Server {
 
         this.get = function(path, _callback) {
             app.get(path, _callback)
+        }
+
+        this.usePrettyUrls = function() {
+            app.get("/*", (req, res) => {
+                if(req.originalUrl == "/") {
+                    res.sendFile(this.dir + "/index.html")
+                }
+                else if(fs.existsSync(this.dir + req.originalUrl + ".html")) {
+                    res.sendFile(this.dir + req.originalUrl + ".html")
+                } else if(fs.existsSync(this.dir + req.originalUrl.replace(".html", "/"))) {
+                    res.redirect(req.originalUrl.replace(".html", "/"))
+                } else {
+                    res.sendStatus(404)
+                }
+            })
         }
     }
 }
